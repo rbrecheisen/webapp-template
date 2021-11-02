b26c3ccf8ea1d5d6255d15807ec245b46aefeb85
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2021-11-02 23:05:50 +0100

    Saving work

--- src/server_v2/app/tasks/checkdicomtask.py
+++ src/server_v2/app/tasks/checkdicomtask.py
@@ -15,26 +15,25 @@ class CheckDicomTask(Task):
         for f in files:
             try:
                 p = pydicom.dcmread(f.path, stop_before_pixels=True)
+                required_tags = [x.strip() for x in task_model.parameters['required_tags'].split(',')]
+                missing_tag = False
+                for tag in required_tags:
+                    if tag not in p:
+                        errors.append('{}: missing tag "{}"'.format(f.path, tag))
+                        missing_tag = True
+                        break
+                if missing_tag:
+                    continue
+                required_rows = int(task_model.parameters['required_rows'])
+                if p.Rows != required_rows:
+                    errors.append('{}: rows {} instead of {}'.format(f.path, p.Rows, required_rows))
+                    continue
+                required_cols = int(task_model.parameters['required_cols'])
+                if p.Columns != required_cols:
+                    errors.append('{}: columns {} instead of {}'.format(f.path, p.Columns, required_cols))
+                    continue
             except pydicom.errors.InvalidDicomError:
-                errors.append('{}: not DICOM'.format(f.path))
-                continue
-            required_tags = [x.strip() for x in task_model.parameters['required_tags'].split(',')]
-            missing_tag = False
-            for tag in required_tags:
-                if tag not in p:
-                    errors.append('{}: missing tag "{}"'.format(f.path, tag))
-                    missing_tag = True
-                    break
-            if missing_tag:
-                continue
-            required_rows = int(task_model.parameters['required_rows'])
-            if p.Rows != required_rows:
-                errors.append('{}: rows {} instead of {}'.format(f.path, p.Rows, required_rows))
-                continue
-            required_cols = int(task_model.parameters['required_cols'])
-            if p.Columns != required_cols:
-                errors.append('{}: columns {} instead of {}'.format(f.path, p.Columns, required_cols))
-                continue
+                pass
         if len(errors) > 0:
             task_model.errors = errors
             task_model.job_status = 'failed'
